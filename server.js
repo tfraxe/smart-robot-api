c1e7920205b45abe763f575cc63c478895b7c9e7
tfraxe <tfraxe@gmail.com>
2020-04-29 21:59:26 -0300

    sakld

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const clarifaiApp = new clarifai.App({
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: process.env.DATABASE_URL,
+		connectionString: process.env.DATABASE_URL,
 		ssl: true
 	}
 });
